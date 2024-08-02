dee6b2e51d03249d47988b68cc0b77ee5bf12487
Daniil <hikiray665@gmail.com>
2024-08-02 16:25:27 +0300

    Initial Commit

---
+++ Buttons, Labels, Gradients/Buttons__Labels__GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buttons__Labels__GradientsApp.swift
+//  Buttons, Labels, Gradients
+//
+//  Created by Даниил Ледник on 02.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Buttons__Labels__GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
